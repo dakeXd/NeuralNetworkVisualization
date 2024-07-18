2df22169a5c21315d666bcc496eccc6c0322b0e3
dakeXd <abarcacrespom@gmail.com>
2024-07-19 00:37:40 +0200

    Gradient Descent

--- NeuralNetworkRepresentation/Assets/Scripts/Functions/DrawFunction.cs
+++ NeuralNetworkRepresentation/Assets/Scripts/Functions/DrawFunction.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class DrawFunction : MonoBehaviour
@@ -9,62 +10,126 @@ public class DrawFunction : MonoBehaviour
     private Color functionColor = new Color(0.7607843F, 1f, 0.6784314F);
     private Color bgColor = new Color(1f, 0.6862745F, 0.6862745F);
     private Color laneColor = new Color(0.8f, 0.8f, 0.9f);
+
+    private List<float> listPositions;
     
     private const int MarginPx = 256;
     private const float MarginDraw = 2;
     public const int TextureSize = 512;
     private float scale = 6f / TextureSize;
     
-    private const int pointSize = 1;
+    //private const int pointSize = 1;
     public Function function;
+    public float learnRate = 0.5f;
+    private const float H = 0.001f;
 
-
+    private float functionScale = 2.9f;
+    //Ball
+    public float xPos, originalXpos;
+    public TextMeshProUGUI textLearnRate;
     private void Start()
     {
+        xPos = -2f;
+        originalXpos = -2f;
+        listPositions = new List<float>();
         function = new Cuadratic();
         texture = new Texture2D(TextureSize, TextureSize);
         Sprite spirte = Sprite.Create(texture, new Rect(0, 0, TextureSize, TextureSize), new Vector2(0.5f, 0.5f), 100);
         GetComponent<SpriteRenderer>().sprite = spirte;
         UpdateTexture();
-        
+      
+    }
+
+    public void SetLearnRate(float newLearn)
+    {
+        learnRate = newLearn;
+        textLearnRate.SetText("Valor de aprendizaje: " + newLearn.ToString("F2"));
+    }
+
+    public void Learn()
+    {
+        float slope = calculateSlope();
+        listPositions.Add(xPos);
+        xPos -= slope * learnRate;
+        UpdateTexture();
+    }
+
+    public float calculateSlope()
+    { 
+        float pos1 = function.F(((xPos + H)) );
+        float pos2 = function.F(((xPos)));
+        float difH = pos1 - pos2;
+        float slope = difH / H;
+        Debug.Log("PosDer: " + pos1 + " PosIzq: "+ pos2+ " Dif: " + difH + " slop: " + slope);
+        return slope;
+
+    }
+    public void Restart()
+    {
+        xPos = originalXpos;
+        listPositions.Clear();
+        UpdateTexture();
+    }
+
+    public void Random()
+    {
+        originalXpos = UnityEngine.Random.Range(-functionScale*0.85f, functionScale*0.85f);
+        xPos = originalXpos;
+        Restart();
     }
     public void UpdateTexture()
+    {
+        DrawBG();
+        DrawF(function, functionColor);
+        for (int i = 0; i < listPositions.Count; i++)
+        {
+            DrawSphere(listPositions[i], function.F(listPositions[i]), 8, Color.gray);
+        }
+        float yPos = function.F(xPos);
+        float slope = calculateSlope();
+        Segment s = new Segment(xPos, yPos, slope, 1);
+        DrawF(s, Color.red);
+        DrawSphere(xPos, yPos, 10, Color.white);
+        texture.Apply();
+    }
+
+    public void DrawBG()
+    {
+        for (int y = 0; y < texture.height; y++)
+        {
+            for (int x = 0; x < texture.width; x++)
+            {
+                bool margin = x <= MarginPx && (x >= (MarginPx - MarginDraw)) ||
+                              (y <= MarginPx && (y >= (MarginPx - MarginDraw)));
+                texture.SetPixel(x, y, margin ? laneColor : bgColor);
+            }
+        }
+    }
+    public void DrawF(Function f, Color color)
     {
         bool first = true;
         int lastY = 0;
         for (int x = 0; x < texture.width; x++)
         {
-            float point = function.F(x * scale- MarginPx * scale );
-            point /= scale;
-            point += MarginPx;
-            int yPoint = Mathf.RoundToInt(point);
+            float point = f.F(normalizeValue(x)); //y normalizado
+            int yPoint = Mathf.RoundToInt(normalziedValueToPixels(point)); //posicion y en la textura
            
             int i1 =0;
             int i2 = 0;
             bool set = false;
-            for (int y = 0; y < texture.height; y++)
+            
+            if ( yPoint >= 0 &&  yPoint <= TextureSize)
             {
-                bool margin = x <= MarginPx && (x >= (MarginPx - MarginDraw)) ||
-                              (y <= MarginPx && (y >= (MarginPx - MarginDraw)));
-
-                    
-                    if (y > yPoint - pointSize && y < yPoint + pointSize)
-                    {
-                        texture.SetPixel(x, y, functionColor);
-                        if (!set)
-                        {
-                            i1 = lastY > y ? y : lastY;
-                            i2 = lastY > y ? lastY : y;
-                            lastY = y;
-                            set = true;
-                        } 
-                        
-                    }
-                    else
-                    {
-                        texture.SetPixel(x, y, margin ? laneColor : bgColor);
-                    }
+                texture.SetPixel(x, yPoint, color);
+                if (!set)
+                {
+                    i1 = lastY > yPoint ? yPoint: lastY;
+                    i2 = lastY > yPoint ? lastY : yPoint;
+                    lastY = yPoint;
+                    set = true;
+                }
             }
+                
             if (set)
             {
                 if (first)
@@ -75,25 +140,56 @@ public class DrawFunction : MonoBehaviour
                 {
                     for (int i = i1; i < i2; i++)
                     {
-                        texture.SetPixel(x, i, functionColor);
+                        texture.SetPixel(x, i, color);
                     }
                 }
             }
         }
-        texture.Apply();
-    }
-
-    public void DrawSlope(float x)
-    {
-        float h = 0.01f;
-        float deltaOut = function.F(x + h) - function.F(x);
-        float slope = deltaOut / h;
-
-        Vector2 slopeDir = new Vector2(1, slope).normalized;
-        Vector2 point = new Vector2(x, function.F(x));
-        
     }
     
+    private void DrawSphere(float normX, float normY, int radius, Color color)
+    {
+        int x = Mathf.RoundToInt(normalziedValueToPixels(normX));
+        int y = Mathf.RoundToInt(normalziedValueToPixels(normY));
+        
+        //Debug.Log(normX + " - " + x);
+        //Debug.Log(normY + " - " + y);
+        for (int i = x - radius; i <= x + radius; i++)
+        {
+            for (int j =y - radius; j <= y + radius; j++)
+            {
+                float distance = Mathf.Sqrt(Mathf.Pow(i - x, 2f ) + Mathf.Pow(j - y, 2f));
+                if (distance <= radius && i < texture.width && y > 0 && j < texture.height && j > 0)
+                {
+                    
+                    if (distance / radius < 0.9f)
+                    {
+                        texture.SetPixel(i, j, color);
+                    }
+                }
+            }
+        }
+ 
+    }
+
+  
+    private float normalizeValue(int v)
+    {
+        return ((float)v - MarginPx) / (TextureSize - MarginPx) * functionScale;
+    }
+    
+    private float normalizeValue(float v)
+    {
+        return (v - MarginPx) / (TextureSize - MarginPx) * functionScale;
+    }
+
+    private float normalziedValueToPixels(float v)
+    {
+        return (v * (TextureSize - MarginPx)/functionScale + MarginPx);
+    }
+ 
+    
+ 
 }
 
 
@@ -107,5 +203,32 @@ public class Cuadratic : Function
     public override float F(float x)
     {
         return 0.2f * x * x * x * x - 0.2f * x * x * x - x * x + .5f;
+    }
+}
+
+public class Segment : Function
+{
+    private float posX, posY, slope, range;
+
+    public Segment(float posX, float posY, float slope, float range)
+    {
+        this.posX = posX;
+        this.posY = posY;
+        this.slope = slope;
+        this.range = range;
+    }
+    
+    public override float F(float x)
+    {
+        
+        float y = slope*(x- posX) + posY;
+        if(Distancia(posX, posY, x, y)> 0.5f)
+            return 9999;
+        return y;
+    }
+
+    public float Distancia(float x1, float y1, float x2, float y2)
+    {
+        return Mathf.Sqrt(Mathf.Pow(x2 - x1, 2) + Mathf.Pow(y2 - y1, 2));
     }
 }
